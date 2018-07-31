d92672acb0d2dbdeb3d03c68e20a6c748ada2f48
EZUHXUA <huazi.yu@ericsson.com>
2018-07-31 15:03:01 +0800

    init

---
+++ blueprints.py
@@ -0,0 +1,21 @@
+from flask import current_app, Blueprint, request
+from settings import TOKEN
+import hashlib
+admin = Blueprint('admin', __name__, url_prefix=None)
+
+@admin.route('/')
+def index():
+    signature = request.args.get('signature', None)
+    timestamp = request.args.get('timestamp', None)
+    nonce = request.args.get('nonce', None)
+    echostr = request.args.get('echostr', None)
+    if check_signature(signature, timestamp, nonce):
+        return echostr
+    return None
+
+def check_signature(signature, timestamp, nonce):
+    token = TOKEN
+
+    if hashlib.sha1(''.join(sorted(token, timestamp, nonce))) == signature:
+        return True
+    return False
