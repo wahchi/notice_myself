984fb2a0de5477aa0a2903ec6e2009f4ded33fcd
EZUHXUA <huazi.yu@ericsson.com>
2018-08-01 15:52:27 +0800

    add add useful api and test receive messages

--- __init__.py
+++ __init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 
-def create_app():
+def create_app(config_file=None):
+
     app = Flask(__name__)
 
+    if app.config['DEBUG']:
+        from . import settings
+        app.config.from_object(settings)
+    else:
+        app.config.from_pyfile(config_file)
     from .blueprints import admin
     app.register_blueprint(admin)
     return app
--- blueprints.py
+++ blueprints.py
@@ -1,19 +1,27 @@
 from flask import current_app, Blueprint, request, json
 from .settings import TOKEN, APPID, APP_SECRET
 import hashlib
-
 import requests
+import xmltodict
 admin = Blueprint('admin', __name__, url_prefix=None)
 
-@admin.route('/')
+@admin.route('/', methods=['GET', 'POST'])
 def index():
-    signature = request.args.get('signature', None)
-    timestamp = request.args.get('timestamp', None)
-    nonce = request.args.get('nonce', None)
-    echostr = request.args.get('echostr', None)
-    if check_signature(signature, timestamp, nonce):
-        return echostr
-    return 'hello'
+    if request.method == 'GET':
+        signature = request.args.get('signature', None)
+        timestamp = request.args.get('timestamp', None)
+        nonce = request.args.get('nonce', None)
+        echostr = request.args.get('echostr', None)
+        if signature or timestamp or nonce:
+
+            if check_signature(signature, timestamp, nonce):
+                return echostr
+        return 'hello'
+    if request.method == 'POST':
+        data = request.data.decode()
+        print(xmltodict.parse(data))
+    return 'helo'
+        # print(data)
 
 def check_signature(signature, timestamp, nonce):
     token = TOKEN
@@ -23,6 +31,7 @@ def check_signature(signature, timestamp, nonce):
 
 @admin.route('/get_access_token')
 def get_access_token():
+    '''获取access_token '''
     token_response = requests.request('GET','https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}'.format(APPID, APP_SECRET))
     access_token, expires_in = "", 0
     if token_response.status_code == 200:
@@ -30,3 +39,128 @@ def get_access_token():
         expires_in = token_response.json()['expires_in']
 
     return json.dumps({'access_token': access_token, 'expires_in': expires_in})
+
+def get_wechat_server_ip():
+    '''获取微信服务器IP地址'''
+    pass
+
+def verify_messages():
+    '''验证消息真实性'''
+    pass
+
+def receive_messages():
+    '''接收普通消息'''
+
+    pass
+
+def receive_events_pass():
+    '''接收事件推送'''
+    pass
+
+def receive_voice_verify():
+    '''接收语音识别结果'''
+    pass
+
+def auto_reply():
+    '''自动回复'''
+    pass
+
+def support_jsapi():
+    '''判断当前客户端版本是否支持指定JS接口'''
+    pass
+
+def get_jsapi_ticket():
+    '''获取jsapi_ticket'''
+    pass
+
+def get_img():
+    '''拍照或从手机相册中选图接口'''
+    pass
+
+def preview_img():
+    '''预览图片接口'''
+    pass
+
+def upload_img():
+    '''上传图片接口'''
+    pass
+
+def download_img():
+    '''下载图片接口'''
+    pass
+
+def start_record():
+    '''开始录音接口'''
+    pass
+
+def stop_record():
+    '''停止录音接口'''
+    pass
+
+def play_voice():
+    '''播放语音接口'''
+    pass
+
+def stop_play():
+    '''暂停播放接口'''
+    pass
+
+def end_play():
+    '''停止播放接口'''
+    pass
+
+def upload_voice():
+    ''''上传语音接口'''
+    pass
+
+def download_voice():
+    '''下载语音接口'''
+    pass
+
+def verify_voice():
+    '''识别音频并返回识别结果接口'''
+    pass
+
+def get_net_status():
+    '''获取网络状态接口'''
+    pass
+
+def get_location():
+    '''获取地理位置接口'''
+    pass
+
+def use_map():
+    '''使用微信内置地图查看位置接口'''
+    pass
+
+def hide_right_corner():
+    '''隐藏右上角菜单接口'''
+    pass
+
+def show_right_corner():
+    '''显示右上角菜单接口'''
+    pass
+
+def close_current_webpage():
+    '''关闭当前网页窗口接口'''
+    pass
+
+def batch_hide_buttons():
+    '''批量隐藏功能按钮接口'''
+    pass
+
+def batch_show_buttons():
+    '''批量显示功能按钮接口'''
+    pass
+
+def hide_all_buttons():
+    '''隐藏所有非基础按钮接口'''
+    pass
+
+def show_all_buttons():
+    '''显示所有功能按钮接口'''
+    pass
+
+def use_scan():
+    '''调起微信扫一扫接口'''
+    pass
